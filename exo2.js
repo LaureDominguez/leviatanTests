d8cd8bef4cd5da0c9ef4c2c52844b379564d12e5
Laure <dominguezlaure@gmail.com>
2023-12-16 14:28:41 +0100

    made exo 1 and 2

--- exo2.js
+++ exo2.js
@@ -1,3 +1,23 @@
 export function Exo2() {
-    console.log('pouet');
+    function afficherReponse(reponse) {
+			var div = document.getElementById('reponse2');
+			var parag = document.createElement('p');
+			parag.textContent = reponse;
+			div.appendChild(parag);
+    }
+    
+    let nombres = [1, 52, 3, 48, 12, 2, 7, 6, 25, 93];
+    let totalPair = 0;
+    for (let i = 0; i < nombres.length; i++){
+        if (nombres[i] % 2 === 0) {
+            totalPair += nombres[i];
+            console.log(nombres[i] + ' est un nombre pair. Résultat total = ' + totalPair);
+            afficherReponse(nombres[i] + ' est un nombre pair. Résultat total = ' + totalPair);
+        } else {
+            console.log(nombres[i]);
+            afficherReponse(nombres[i]);
+        }
+    }
+    console.log('Résulat final = ' + totalPair);
+    afficherReponse('Résulat final = ' + totalPair);
 }
--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 import { Exo1 } from "./exo1.js";
 import { Exo2 } from "./exo2.js";
 
-Exo1();
+// Exo1();
 Exo2();
